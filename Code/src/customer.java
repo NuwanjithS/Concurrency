c40a4d236107f83e515293f89d2f5ae13f742be4
NuwanjithS <nuwanjiths@fortude.co>
2024-03-31 14:37:37 +0530

    updated customer

--- Code/src/customer.java
+++ Code/src/customer.java
@@ -1,3 +1,9 @@
 public class Customer {
+    private String customerID;
+    private String firstName;
+    private String lastName;
+    private String contactNo;
+    private String address;
+    private String nicNo;
 
 }
